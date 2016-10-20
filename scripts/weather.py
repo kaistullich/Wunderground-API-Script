c3442ff3b417e61e3ddf22c7ed48fc8d48184b91
Kai Stullich <kaistullich@rocketmail.com>
2016-10-20 15:00:36 -0700

    Added a loop and an option for user to quit the program, along with some more documentation

--- scripts/weather.py
+++ scripts/weather.py
@@ -7,34 +7,43 @@ import sys
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
-# Allows user to set the State in which they want to search
-user_state = raw_input("Please enter the State's abbreviation (i.e. CA, NV, etc): ")
-# This will auto capitalize the abbreviation, if user did not do so already (i.e. ca = CA)
-user_state_upper = user_state.upper()
+choice = ""
+
+while True:
+    # Allows user to set the State in which they want to search
+    user_state = raw_input("\nPlease enter the State's abbreviation (i.e. CA, NV, etc): ")
+    # This will auto capitalize the abbreviation, if user did not do so already (i.e. ca = CA)
+    user_state_upper = user_state.upper()
 
 
 
-# Allows the user to specify which City they want to search from within the State
-user_city = raw_input('Please enter the City of the State you are inquiring about: ')
-# This variable will replace any white space for Cities with 2 words, 
-# and add an Underscore (i.e. San Francisco = San_Francisco)
-user_city_replace = user_city.replace(" ", "_")
-# This variable will Auto-Capitalize the city (i.e. san francisco = San_Francisco)
-user_replace_title = user_city_replace.title()
+    # Allows the user to specify which City they want to search from within the State
+    user_city = raw_input('Please enter the City of the State you are inquiring about: ')
+    # This variable will replace any white space for Cities with 2 words, 
+    # and add an Underscore (i.e. San Francisco = San_Francisco)
+    user_city_replace = user_city.replace(" ", "_")
+    # This variable will Auto-Capitalize the city (i.e. san francisco = San_Francisco)
+    user_replace_title = user_city_replace.title()
 
 
 
-# URL that leads to Wundergound weather
-wunderground_url = urllib2.urlopen('http://api.wunderground.com/api/ed52388436c43bbe/geolookup/conditions/q/%s/%s.json' % (user_state,user_replace_title))
+    # URL that leads to Wundergound weather
+    wunderground_url = urllib2.urlopen('http://api.wunderground.com/api/ed52388436c43bbe/geolookup/conditions/q/%s/%s.json' % (user_state,user_replace_title))
 
-json_string = wunderground_url.read()
+    json_string = wunderground_url.read()
 
-parsed_json = json.loads(json_string)
+    parsed_json = json.loads(json_string)
 
-location = parsed_json['location']['city']
+    location = parsed_json['location']['city']
 
-temp_f = parsed_json['current_observation']['temp_f']
+    temp_f = parsed_json['current_observation']['temp_f']
 
-print ("\nThe current temperature in %s is: %s °F" % (location, temp_f))
-# Exit the script
-wunderground_url.close()
+    print ("\nThe current temperature in %s is: %s °F" % (location, temp_f))
+    
+    # User is able to choose wether they want to EXIT or continue the program
+    user_choice = raw_input('\nWould you like to EXIT the program? (y/n): ')
+    if user_choice == "n":
+        pass
+    #This will EXIT the program
+    if user_choice == "y":
+        break
