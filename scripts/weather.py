06e5e9f9a33b7a6c859c07de44946fdbc4967e09
Kai Stullich <kaistullich@rocketmail.com>
2016-10-19 18:24:27 -0700

    Added user ability to specify which State and City to search in

--- scripts/weather.py
+++ scripts/weather.py
@@ -1,7 +1,17 @@
 import urllib2
 import json
 
-print ('hello my name is Kai')
+user_state = raw_input("Please enter the State's abbreviation (i.e. CA, NV, etc): ")
+
+user_state_upper = user_state.upper()
+
+user_city = raw_input('Please enter the City of the State you are inquiring about: ')
+
+user_city_replace = user_city.replace(" ", "_")
+
+user_replace_title = user_city_replace.title()
+
+
 wunderground_url = urllib2.urlopen('http://api.wunderground.com/api/ed52388436c43bbe/geolookup/conditions/q/CA/Morgan_Hill.json')
 
 json_string = wunderground_url.read()
@@ -15,3 +25,4 @@ temp_f = parsed_json['current_observation']['temp_f']
 print ("Current temperature in %s is: %s" % (location, temp_f))
 
 wunderground_url.close()
+
