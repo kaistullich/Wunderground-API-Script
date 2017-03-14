314df09bd7aba3c4ef82c8aabd47cba6650992ea
kaistullich <kairstullich@gmail.com>
2017-03-14 12:46:05 -0700

    added try/except KeyError & NameError blocks

--- scripts/weather.py
+++ scripts/weather.py
@@ -2,17 +2,23 @@ import requests
 import json
 
 while True:
-    user_state = input("\nPlease enter the State's abbreviation (i.e. CA, NV, etc): ").upper()
-    user_city = input('Please enter the City of the State you are inquiring about: ').replace(" ", "_").title()
-    wunderground_url = requests.get('http://api.wunderground.com/api/ed52388436c43bbe/geolookup/conditions/q/{st}/ \
-                                    {st_r}.json'.format(st=user_state, st_r=user_city))
+    try:
+        user_state = input("\nPlease enter the State's abbreviation (i.e. CA, NV, etc): ").upper()
+        user_city = input('Please enter the City of the State you are inquiring about: ').replace(" ", "_").title()
+        wunderground_url = requests.get('http://api.wunderground.com/api/ed52388436c43bbe/geolookup/conditions/q/{st}/ \
+                                        {st_r}.json'.format(st=user_state, st_r=user_city))
 
-    json_string = wunderground_url.text
-    parsed_json = json.loads(json_string)
-    location = parsed_json['location']['city']
-    temp_f = parsed_json['current_observation']['temp_f']
+        json_string = wunderground_url.text
+        parsed_json = json.loads(json_string)
+        location = parsed_json['location']['city']
+        temp_f = parsed_json['current_observation']['temp_f']
+        print("\nThe current temperature in {loc} is: {deg} °F".format(loc=location, deg=temp_f))
 
-    print("\nThe current temperature in {loc} is: {deg} °F".format(loc=location, deg=temp_f))
+    except KeyError:
+        print('System did not return a proper response, please try again.')
+
+    except NameError:
+        print('System did not return a proper response, please try again.')
 
     while True:
         accepted_no_answer = ['no', 'n']
