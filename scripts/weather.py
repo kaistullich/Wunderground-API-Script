f32f285c7b6593363d0c2ed24c0f4c2fb98d2ab1
kaistullich <kairstullich@gmail.com>
2017-03-14 12:30:13 -0700

    removed comments

--- scripts/weather.py
+++ scripts/weather.py
@@ -2,16 +2,8 @@ import requests
 import json
 
 while True:
-    # Allows user to set the State in which they want to search
-    user_state = input("\nPlease enter the State's abbreviation (i.e. CA, NV, etc): ")
-
-    # This will auto capitalize the abbreviation, if user did not do so already (i.e. ca = CA)
-    user_state_upper = user_state.upper()
-
-    # Allows the user to specify which City they want to search from within the State
+    user_state = input("\nPlease enter the State's abbreviation (i.e. CA, NV, etc): ").upper()
     user_city = input('Please enter the City of the State you are inquiring about: ').replace(" ", "_").title()
-
-    # URL that leads to Wundergound weather
     wunderground_url = requests.get('http://api.wunderground.com/api/ed52388436c43bbe/geolookup/conditions/q/{st}/ \
                                     {st_r}.json'.format(st=user_state, st_r=user_city))
 
@@ -22,7 +14,6 @@ while True:
 
     print("\nThe current temperature in {loc} is: {deg} °F".format(loc=location, deg=temp_f))
 
-    # User is able to choose whether they want to EXIT or continue the program
     while True:
         accepted_no_answer = ['no', 'n']
         accepted_yes_answer = ['yes', 'y']
